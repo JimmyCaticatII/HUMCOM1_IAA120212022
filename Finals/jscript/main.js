3b614164033743f55d9af1ebdd9e032862d710b9
Git Gud <20161084@s.ubaguio.edu>
2022-05-03 11:23:09 +0800

    Lesson14.html

---
+++ Finals/jscript/main.js
@@ -0,0 +1,22 @@
+var myName = "Jimmy";
+console.log("hello " + myName);
+
+const idNumber = "20161084";
+console.log("ID Number: " + idNumber);
+
+let age = 18;
+    console.log("Age: " + age);
+    age = 19;
+    console.log("Age: " + age);
+
+var species = "Human";
+
+function transform() {
+    var species = "Werewolf";
+    console.log(species)
+}
+
+console.log(species);
+transform();
+console.log(species);
+
