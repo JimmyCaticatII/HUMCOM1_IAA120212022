c776772f502a4fa129f338e599ac2ede82f54f73
Git Gud <20161084@s.ubaguio.edu>
2022-05-07 19:20:42 +0800

    Exercise15

---
+++ Finals/jscript/Script15.js
@@ -0,0 +1,15 @@
+const myName = "Jimmy Caticat II", myAge = 23;
+document.getElementById("pname").innerHTML = myName;
+document.getElementById("page").innerHTML = myAge;
+
+const myNum = '09876543211', myEmail = "20161084@s.ubaguio.edu", myMailad = 
+"Assumption Rd., Baguio City";
+document.getElementById("cnum").innerHTML = myNum;
+document.getElementById("cemail").innerHTML = myEmail;
+document.getElementById("cmailad").innerHTML = myMailad;
+
+const myProg = "Computer Science", myID = 20161084, myYl = "1st Year", mySec = "IAA1";
+document.getElementById("aprog").innerHTML = myProg;
+document.getElementById("aid").innerHTML = myID;
+document.getElementById("ayl").innerHTML = myYl;
+document.getElementById("asec").innerHTML = mySec;
